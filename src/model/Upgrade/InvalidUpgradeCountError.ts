98873ff28e9cc2c100b20bc280544b4b22bda3ee
MoulikB <moulikbhatia@icloud.com>
2026-02-07 11:51:56 -0600

    Removed unneeded error

--- src/model/Upgrade/InvalidUpgradeCountError.ts
+++
@@ -1,6 +0,0 @@
-export class InvalidUpgradeCountError extends Error {
-  constructor(message: string = "Invalid Error Count") {
-    super(message);
-    this.name = "InvalidUpgradeCountError";
-  }
-}
