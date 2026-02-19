e53cebe4c6718323454dba27cc70e6efb0ae27ad
MoulikB <moulikbhatia@icloud.com>
2026-02-19 16:39:14 -0600

    d

--- create-tables.sql
+++ create-tables.sql
@@ -1,23 +1,21 @@
 create table if not exists player (
-    id serial primary key,
-    name varchar(255) not null unique,
+    name varchar(255) primary key,
     badCode integer not null default 0,
     clickPower integer not null default 1
 );
 
 create table if not exists upgrade_type (
-    id serial primary key,
-    name varchar(255) not null unique,
+    name varchar(255) primary key,
     baseCost integer not null,
     clickPowerIncrease integer not null
 );
 
 create table if not exists player_upgrade (
     id serial primary key,
-    player_id integer not null,
-    upgrade_type_id integer not null,
+    player_name varchar(255) not null,
+    upgrade_name varchar(255) not null,
     level integer not null default 0,
-    foreign key (player_id) references player(id) on delete cascade,
-    foreign key (upgrade_type_id) references upgrade_type(id),
-    unique (player_id, upgrade_type_id)
+    foreign key (player_name) references player(name) on delete cascade,
+    foreign key (upgrade_name) references upgrade_type(name),
+    unique (player_name, upgrade_name)
 );
